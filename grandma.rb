c17081f7fa43671181bf408c5e6545aa2f63c615
William Sanchez <williamsanchez.8055@gmail.com>
2019-12-27 04:19:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
   "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase 
+  else 
   "NO, NOT SINCE 1938!"
 end
 end
