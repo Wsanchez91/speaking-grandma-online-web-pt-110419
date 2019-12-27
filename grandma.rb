69fac58e82676bb34fd9f5fcfde69f33603e5d0c
William Sanchez <williamsanchez.8055@gmail.com>
2019-12-27 04:04:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ if phrase.upcase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?" || "HI!"
   "HUH?! SPEAK UP, SONNY!"
-  else phrase.upcase"Hi Nana, how are you?" || "HI!"
+  else phrase == phrase.upcase "Hi Nana, how are you?" || "HI!"
   "NO, NOT SINCE 1938!"
 end
 end
