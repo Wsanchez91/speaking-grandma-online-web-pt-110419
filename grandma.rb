fe760ec159f94873a095d9ee05b74eb5abd6de09
William Sanchez <williamsanchez.8055@gmail.com>
2019-12-27 03:36:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,15 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN"
-else 
-  "HUH?! SPEAK UP, SONNY"
+
+if phrase.upcase == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+  elsif "hi grandma"
+  "HUH?! SPEAK UP, SONNY!"
+  elsif "HI!"
+  "HUH?! SPEAK UP, SONNY!"
+  elsif "what did you eat today?"
+  "NO, NOT SINCE 1938!"
+  elsif "WHAT!"
+  "NO, NOT SINCE 1938!"
 end
 
-speak_to_grandma("I LOVE YOU GRANDMA!")
+speak_to_grandma("i love you grandma!")
