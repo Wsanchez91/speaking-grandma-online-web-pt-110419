af2707f817a0c95fc2e7914b808f10642062654e
William Sanchez <williamsanchez.8055@gmail.com>
2019-12-27 04:23:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
 
 if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase != phrase.upcase
   "HUH?! SPEAK UP, SONNY!"
   else 
     "NO, NOT SINCE 1938!"
